fcdad6fdef6fefd91b30e70452625f0a81022a89
Kelly <kelly.a.artola@ricealumni.net>
2022-08-01 17:55:47 -0500

    edit review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,6 +4,18 @@ const router = express.Router();
 
 const { Image, Review } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Review text is required"),
+  check("stars")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Stars must be an integer from 1 to 5"),
+  handleValidationErrors,
+];
 
 router.post("/:reviewId/images", requireAuth, async (req, res) => {
   const { user } = req;
@@ -49,6 +61,37 @@ router.post("/:reviewId/images", requireAuth, async (req, res) => {
       id,
       imageableId,
       url,
+    });
+  }
+});
+
+router.put("/:reviewId", requireAuth, validateReview, async (req, res) => {
+  const { reviewId } = req.params;
+  const { review, stars } = req.body;
+  const { user } = req;
+
+  const reviews = await Review.findByPk(groupId);
+
+  if (!reviews) {
+    res.status(404);
+    return res.json({
+      message: "Review couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  if (user.id === reviews.userId) {
+    await reviews.update({
+      review,
+      stars,
+    });
+
+    return res.json(reviews);
+  } else {
+    res.status(403);
+    return res.json({
+      message: "Forbidden",
+      statusCode: 403,
     });
   }
 });
