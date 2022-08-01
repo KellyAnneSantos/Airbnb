503b88009b1bf7c81eee70e4ee77e0aef2577edd
Kelly <kelly.a.artola@ricealumni.net>
2022-07-31 23:34:25 -0500

    requireauth and commenting out unused routers

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,21 +2,21 @@ const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
-const reviewsRouter = require("./reviews.js");
-const bookingsRouter = require("./bookings.js");
-const imagesRouter = require("./images.js");
+// const reviewsRouter = require("./reviews.js");
+// const bookingsRouter = require("./bookings.js");
+// const imagesRouter = require("./images.js");
 
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
-router.use("/spots", eventsRouter);
+router.use("/spots", spotsRouter);
 
-// router.use("/reviews", groupsRouter);
+// router.use("/reviews", reviewsRouter);
 
-// router.use("/bookings", imagesRouter);
+// router.use("/bookings", bookingsRouter);
 
-// router.use("/images", venuesRouter);
+// router.use("/images", imagesRouter);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
