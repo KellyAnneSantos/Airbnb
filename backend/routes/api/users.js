aead092a117232db9d6b149ca0c9e22c59b7d73b
Kelly <kelly.a.artola@ricealumni.net>
2022-07-31 23:57:03 -0500

    require Image, requireAuth and sequelize

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { Image, User, Spot } = require("../../db/models");
+const { Image, User, Spot, sequelize } = require("../../db/models");
 
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
