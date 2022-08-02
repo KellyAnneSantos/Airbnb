8777f5583035540c800c906a9682397968b118b8
Kelly <kelly.a.artola@ricealumni.net>
2022-08-01 20:44:36 -0500

    uncommented imagesRouter

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,7 @@ const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewsRouter = require("./reviews.js");
 const bookingsRouter = require("./bookings.js");
-// const imagesRouter = require("./images.js");
+const imagesRouter = require("./images.js");
 
 router.use(sessionRouter);
 
