64baed800d88efdf733bc084a746e090699708ca
Kelly <kelly.a.artola@ricealumni.net>
2022-08-01 21:12:17 -0500

    require Op

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,6 +10,7 @@ const {
   User,
   sequelize,
 } = require("../../db/models");
+const { Op } = require("sequelize");
 const { requireAuth } = require("../../utils/auth");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
